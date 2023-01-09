e4e5b4936860175e20859d740e49e2a576d30f13
Mohit Bhansali <mohit@nanovest.io>
2023-01-09 11:42:09 +0800

    Added page title and NIST content

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import { useRouter } from 'next/router'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
+  useNextSeoProps() {
+    const { route } = useRouter()
+    if (route !== '/') {
+      return {
+        titleTemplate: '%s – Cybersecurity'
+      }
+    }
+  },
   logo: <span>Cybersecurity</span>,
   project: {
     link: 'https://github.com/mohitbhansali/cybersecurity',
